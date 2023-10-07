08cd2707e543e6712b8f2d337014b89ad8179115
seongminn <berry9901@gmail.com>
2023-10-07 13:19:33 +0900

    delete: 사용하지 않는 파일 제거

--- src/components/home/GameItem/GameLabel.tsx
+++
@@ -1,7 +0,0 @@
-interface GameLabelProps {
-  children?: string;
-}
-
-export default function GameLabel({ children }: GameLabelProps) {
-  return <p className='w-full text-sm text-center'>{children}</p>;
-}
