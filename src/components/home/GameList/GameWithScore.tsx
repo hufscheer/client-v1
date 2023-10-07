8edf61fd326e83eb376946c522eb765436a81404
seongminn <berry9901@gmail.com>
2023-10-07 13:18:23 +0900

    feat: 게임 컴포넌트를 조합한 게임 리스트 생성

---
+++ src/components/home/GameList/GameWithScore.tsx
@@ -0,0 +1,38 @@
+import { AllGamesResponse } from '@/types/game';
+import { Game } from '../Game';
+
+interface GameListProps {
+  data: AllGamesResponse[];
+}
+
+export default function GameList({ data }: GameListProps) {
+  return (
+    <ul>
+      {data.map((game) => (
+        <Game key={game.id}>
+          <Game.Label>{game.name}</Game.Label>
+          <Game.TeamWrapper gap={2}>
+            <Game.TeamLogo
+              src={game.firstTeam.logoImageUrl}
+              alt={`${game.firstTeam.name} 로고`}
+            />
+            <Game.TeamName>{game.firstTeam.name}</Game.TeamName>
+          </Game.TeamWrapper>
+          <Game.Status>
+            <Game.Score
+              firstTeamScore={game.firstTeamScore}
+              secondTeamScore={game.secondTeamScore}
+            />
+          </Game.Status>
+          <Game.TeamWrapper reverse gap={2}>
+            <Game.TeamLogo
+              src={game.firstTeam.logoImageUrl}
+              alt={`${game.secondTeam.name} 로고`}
+            />
+            <Game.TeamName>{game.secondTeam.name}</Game.TeamName>
+          </Game.TeamWrapper>
+        </Game>
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/home/GameList/GameWithTimeStamp.tsx
@@ -0,0 +1,35 @@
+import { AllGamesResponse } from '@/types/game';
+import { Game } from '../Game';
+
+interface GameListProps {
+  data: AllGamesResponse[];
+}
+
+export default function GameWithTimeStamp({ data }: GameListProps) {
+  return (
+    <ul>
+      {data.map((game) => (
+        <Game key={game.id}>
+          <Game.Label>{game.name}</Game.Label>
+          <Game.TeamWrapper gap={2}>
+            <Game.TeamLogo
+              src={game.firstTeam.logoImageUrl}
+              alt={`${game.firstTeam.name} 로고`}
+            />
+            <Game.TeamName>{game.firstTeam.name}</Game.TeamName>
+          </Game.TeamWrapper>
+          <Game.Status>
+            <Game.Timer date={game.startTime} />
+          </Game.Status>
+          <Game.TeamWrapper reverse gap={2}>
+            <Game.TeamLogo
+              src={game.firstTeam.logoImageUrl}
+              alt={`${game.secondTeam.name} 로고`}
+            />
+            <Game.TeamName>{game.secondTeam.name}</Game.TeamName>
+          </Game.TeamWrapper>
+        </Game>
+      ))}
+    </ul>
+  );
+}
