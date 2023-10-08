b52654dd608a83557101a02d51fa6625f346213a
HiImConan <mindol0320@naver.com>
2023-10-08 16:00:09 +0900

    feat: 경기 상세 데이터 apicall 함수 추가

--- src/api/game.ts
+++ src/api/game.ts
@@ -1,6 +1,23 @@
-import { AllGamesResponse } from '@/types/game';
+import { AllGamesResponse, EachGameResponse } from '@/types/game';
 import instance from './instance';
+import { AxiosError, AxiosResponse } from 'axios';
 
 export const getAllGames = async () => {
   return instance.get<AllGamesResponse[]>('/games');
+};
+
+export const getEachGame = async (gameID: number) => {
+  try {
+    const response: AxiosResponse<EachGameResponse> = await instance.get(
+      `/games/${gameID}`,
+    );
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    if (axiosError.response) {
+      return axiosError.response.status;
+    } else {
+      throw new Error();
+    }
+  }
 };
