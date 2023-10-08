4b480edebb7694a47704079289759b120b07121d
HiImConan <mindol0320@naver.com>
2023-10-09 01:52:09 +0900

    style: 배경색 slate-50

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className} max-w-sm m-auto`}>
+    <html lang="en">
+      <body className={`${inter.className} max-w-sm m-auto bg-slate-50`}>
         <Header />
         {children}
         <Footer />
