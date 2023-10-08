901818c7a9af821af9f212daf10ed867851d7ea4
HiImConan <mindol0320@naver.com>
2023-10-09 06:02:11 +0900

    feat: 조건부 수정 버튼 추가

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,22 +1,46 @@
+'use client';
+
 import { getAllGames, getEachGame } from '@/api/game';
 import GameComments from '@/components/detail/GameComments';
 import GameInfo from '@/components/detail/GameInfo';
 import GameTimeline from '@/components/detail/GameTimeline';
+import { EachGameResponse } from '@/types/game';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-export default async function detailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default function detailPage({ params }: { params: { id: string } }) {
+  const [gameData, setGameData] = useState<EachGameResponse>();
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
   const gameID = params.id;
-  const gameData = await getEachGame(Number(gameID));
-  if (typeof gameData === 'number') return notFound();
+
+  useEffect(() => {
+    const getGameData = async () => {
+      const res = await getEachGame(Number(gameID));
+      if (typeof res === 'number') return notFound();
+      setGameData(res);
+    };
+    getGameData();
+    const token = localStorage.getItem('token');
+    token && setIsLoggedIn(true);
+  }, []);
+
   return (
     <div className="flex flex-col gap-8">
-      <GameInfo game={gameData} />
-      <GameTimeline records={gameData.records} status={gameData.gameStatus} />
-      <GameComments gameID={gameData.id} />
+      {isLoggedIn && (
+        <Link
+          href={`/detail/${gameID}/modify`}
+          className="p-2 rounded-lg border border-red-500 w-fit bg-red-400 text-white"
+        >
+          수정
+        </Link>
+      )}
+      {gameData && <GameInfo game={gameData} />}
+      {gameData && (
+        <GameTimeline records={gameData.records} status={gameData.gameStatus} />
+      )}
+      {gameData && <GameComments gameID={gameData.id} />}
     </div>
   );
 }
