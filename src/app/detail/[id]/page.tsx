2b6cc1be26a54965a7f29dcc86b601f72961808e
HiImConan <mindol0320@naver.com>
2023-10-08 20:02:27 +0900

    feat: detail 페이지 제작

---
+++ src/app/detail/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { getEachGame } from '@/api/game';
+import GameComments from '@/components/detail/GameComments';
+import GameInfo from '@/components/detail/GameInfo';
+import GameTimeline from '@/components/detail/GameTimeline';
+import { notFound } from 'next/navigation';
+
+export default async function detailPage({
+  params,
+}: {
+  params: { id: number };
+}) {
+  const gameID = params.id;
+  const gameData = await getEachGame(gameID);
+  if (typeof gameData === 'number') return notFound();
+  return (
+    <>
+      <GameInfo game={gameData} />
+      <GameTimeline records={gameData.records} status={gameData.gameStatus} />
+      <GameComments gameID={gameData.id} />
+    </>
+  );
+}
