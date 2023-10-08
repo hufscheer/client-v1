d71c0a881358cac17df1c9e41b80a4115afd8140
seongminn <berry9901@gmail.com>
2023-10-08 16:23:04 +0900

    chore: Game 컴포넌트 위치 변경에 따른 주소 수정

--- src/components/home/Game/GameTeamName.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-interface GameTeamNameProps {
-  children?: ReactNode;
-}
-
-export default function GameTeamName({ children }: GameTeamNameProps) {
-  return <p>{children}</p>;
-}
