2fc849e30ca5b95f8d06871dced1122f56b6a699
seongminn <berry9901@gmail.com>
2023-10-08 19:24:36 +0900

    feat: 게임 등록 api 추가 및 pathname에 따라 BASE_URL 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://3.36.83.80:8080/';
+const BASE_URL =
+  window.location.pathname === '/admin'
+    ? process.env.NEXT_PUBLIC_BACK_OFFICE_BASE_URL
+    : process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // 토큰? 세션?
-const token = '_secret';
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5MzIxOTQyLCJpYXQiOjE2OTY3Mjk5NDIsImp0aSI6IjMxZDljYmI3MmM2ZTRiYjdhMGI5N2E3MTY2MDJhZjlkIiwidXNlcl9pZCI6MX0.I2YYjSt844HIZ9aG4MCwM1ucUy5-vTMrKnZWVsZn1-M';
 
 const instance = axios.create({
   baseURL: BASE_URL,
