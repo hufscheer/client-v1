f9d66681f04d2d1bc93844c00c6fde78439a908d
seongminn <berry9901@gmail.com>
2023-10-11 01:59:48 +0900

    fix: 정확한 에러 확인을 위해 try-catch문으로 api 호출 핸들링

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,5 +1,7 @@
 import { GameTeamProps } from '@/types/game';
 import { adminInstance } from './instance';
+import { AxiosError, AxiosResponse } from 'axios';
+import * as Sentry from '@sentry/nextjs';
 
 type CreateGameRequest = {
   name: string;
@@ -21,9 +23,33 @@ type GameScoreRequest = {
 };
 
 export const createGame = (body: CreateGameRequest) => {
-  return adminInstance.post('/manage/game/register/', body);
+  try {
+    return adminInstance.post('/manage/game/register/', body);
+  } catch (error) {
+    const axiosError = error as AxiosError;
+
+    Sentry.captureException(axiosError);
+
+    if (axiosError.response) {
+      throw new Error(axiosError.response.statusText);
+    } else {
+      throw new Error('경기를 생성하는 데에 실패했습니다!');
+    }
+  }
 };
 
 export const postGameScore = (id: number, body: GameScoreRequest) => {
-  return adminInstance.post(`/manage/game/score/${id}/`, body);
+  try {
+    return adminInstance.post(`/manage/game/score/${id}/`, body);
+  } catch (error) {
+    const axiosError = error as AxiosError;
+
+    Sentry.captureException(axiosError);
+
+    if (axiosError.response) {
+      throw new Error(axiosError.response.statusText);
+    } else {
+      throw new Error('경기 점수를 변경하는 데에 실패했습니다!');
+    }
+  }
 };
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,28 @@
 import { PostLoginRequest, PostLoginResponse } from '@/types/auth';
 import { adminInstance } from './instance';
 import { GameStatusType } from '@/types/game';
+import { AxiosError, AxiosResponse } from 'axios';
+import * as Sentry from '@sentry/nextjs';
 
 export const postLogin = async (body: PostLoginRequest) => {
-  return adminInstance.post<PostLoginResponse>('/accounts/login/', body);
+  try {
+    const response: AxiosResponse<PostLoginResponse> = await adminInstance.post(
+      '/accounts/login/',
+      body,
+    );
+
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+
+    Sentry.captureException(axiosError);
+
+    if (axiosError.response) {
+      throw new Error(axiosError.response.statusText);
+    } else {
+      throw new Error('팀 목록을 불러오는 데에 실패했습니다!');
+    }
+  }
 };
 
 export const postGameStatus = async (
