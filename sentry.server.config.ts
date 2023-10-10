045c93ffe50c3460fb6b6bcafa53801a86910221
seongminn <berry9901@gmail.com>
2023-10-11 01:59:12 +0900

    chore: 센트리 설치

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://cc1dcf1845d69a9079c615d462e122b2@o4506026798088192.ingest.sentry.io/4506026816503808',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 0.2,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
