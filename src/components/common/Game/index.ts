9711ada398f04dbd74aea753a486e827f83345c4
seongminn <berry9901@gmail.com>
2023-11-12 18:03:50 +0900

    chore: 상태명 변경에 따른 props 변경

--- src/components/common/Game/index.ts
+++ src/components/common/Game/index.ts
@@ -1,20 +1,12 @@
-import GameLabel from './GameLabel';
-import GameLive from './GameLive';
-import GameMain from './GameMain';
-import GameScore from './GameScore';
-import GameStatus from './GameStatus';
-import GameTeam from './GameTeam';
-import GameTeamLogo from './GameTeamLogo';
-import GameTeamName from './GameTeamName';
-import GameTimer from './GameTimer';
+import GameWrapper from './GameWrapper';
+import Label from './Label';
+import Score from './Score';
+import Status from './Status';
+import Team from './Team';
 
-export const Game = Object.assign(GameMain, {
-  Label: GameLabel,
-  Live: GameLive,
-  Score: GameScore,
-  Status: GameStatus,
-  TeamWrapper: GameTeam,
-  TeamLogo: GameTeamLogo,
-  TeamName: GameTeamName,
-  Timer: GameTimer,
+export const Game = Object.assign(GameWrapper, {
+  Label,
+  Score,
+  Status,
+  Team,
 });
--- src/components/home/GameList/GameWithScore.tsx
+++ src/components/home/GameList/GameWithScore.tsx
@@ -1,44 +1,15 @@
-import Link from 'next/link';
-
-import { Game } from '@/components/common/Game';
-import { GameType } from '@/types/game';
+import GameItem from '@/components/home/GameItem';
+import { GameDetailType } from '@/types/game';
 
 interface GameListProps {
-  data: GameType[];
+  data: GameDetailType[];
 }
 
 export default function GameList({ data }: GameListProps) {
   return (
     <ul className="space-y-4">
       {data.map(game => (
-        <li key={game.id}>
-          <Link
-            href={`/detail/${game.id}`}
-            className="flex flex-col gap-1 justify-center p-2 rounded-lg shadow-md bg-white"
-          >
-            <Game>
-              <Game.Label>{game.name}</Game.Label>
-              <Game.TeamWrapper gap={2}>
-                <Game.TeamLogo
-                  src={game.firstTeam.logoImageUrl}
-                  alt={`${game.firstTeam.name} 로고`}
-                />
-                <Game.TeamName>{game.firstTeam.name}</Game.TeamName>
-              </Game.TeamWrapper>
-              <Game.Score
-                firstTeamScore={game.firstTeamScore}
-                secondTeamScore={game.secondTeamScore}
-              />
-              <Game.TeamWrapper reverse gap={2}>
-                <Game.TeamLogo
-                  src={game.secondTeam.logoImageUrl}
-                  alt={`${game.secondTeam.name} 로고`}
-                />
-                <Game.TeamName>{game.secondTeam.name}</Game.TeamName>
-              </Game.TeamWrapper>
-            </Game>
-          </Link>
-        </li>
+        <GameItem key={game.id} {...game} />
       ))}
     </ul>
   );
