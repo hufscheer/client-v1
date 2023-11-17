29d61908566dd614eb905fe1f4f6cbcf1366df2f
HiImConan <mindol0320@naver.com>
2023-11-17 18:09:14 +0900

    chore: blue, gray 색상 팔레트 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          light: '#D3E2F6',
+          DEFAULT: '#4354F0',
+          heavy: '#303ECE',
+        },
+        gray: {
+          extralight: '#F2F4F6',
+          light: '#E9E9E9',
+          DEFAULT: '#D9D9D9',
+          heavy: '#757575',
+          extraheavy: '#4E5968'
+        },
+      },
+    },
   },
   plugins: [],
 };
