13aec7f5cb0df14286a1492b66c8d765b8d4feed
HiImConan <mindol0320@naver.com>
2023-11-18 14:14:09 +0900

    chore: iconcolor 삭제 및 카멜케이스 적용

--- src/components/common/Icon/icon.type.ts
+++ src/components/common/Icon/icon.type.ts
@@ -1,4 +1,3 @@
-import { ICON_COLOR, ICON_NAME } from '@/components/common/Icon/IconMap';
+import { ICON_NAME } from '@/components/common/Icon/IconMap';
 
 export type IconName = (typeof ICON_NAME)[keyof typeof ICON_NAME];
-export type IconColor = (typeof ICON_COLOR)[keyof typeof ICON_COLOR];
