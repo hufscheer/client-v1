401a6b77d6a801c0b58664cea46313a57b0ecbf0
HiImConan <mindol0320@naver.com>
2023-11-18 23:37:30 +0900

    refactor: 불필요한 타입 단언 삭제  및 icon 이름 유니온 타입 수정

--- src/components/common/Icon/icon.type.ts
+++ src/components/common/Icon/icon.type.ts
@@ -1,3 +1,3 @@
-import { ICON_NAME } from '@/components/common/Icon/IconMap';
+import { iconMap } from '@/components/common/Icon/IconMap';
 
-export type IconName = (typeof ICON_NAME)[keyof typeof ICON_NAME];
+export type IconName = keyof typeof iconMap;
