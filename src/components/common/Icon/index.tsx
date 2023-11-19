fe7f87c5e5d98a5643cdc166c10ccd94d068b711
HiImConan <mindol0320@naver.com>
2023-11-19 12:02:36 +0900

    chore: svg 누락 props 추가

--- src/components/common/Icon/index.tsx
+++ src/components/common/Icon/index.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps } from 'react';
 
+import { $ } from '@/utils/core';
+
 import { IconName } from './icon.type';
 import { iconMap } from './IconMap';
 
@@ -7,13 +9,22 @@ interface IconProps extends ComponentProps<'svg'> {
   iconName: IconName;
 }
 
-export const Icon = ({ iconName, ...props }: IconProps) => {
+export const Icon = ({
+  iconName,
+  width = 24,
+  height = 24,
+  className,
+  ...props
+}: IconProps) => {
   const path = iconMap[iconName];
   return (
     <svg
+      width={width}
+      height={height}
       fillRule="evenodd"
       clipRule="evenodd"
       xmlns="http://www.w3.org/2000/svg"
+      className={$(className)}
       {...props}
     >
       <path d={path} />
