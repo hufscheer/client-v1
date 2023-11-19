e0f43c482beae47b4dd80d127fe5755c4b60bd9f
HiImConan <mindol0320@naver.com>
2023-11-19 11:25:29 +0900

    chore: 누락된 width, height, classname props 추가

--- src/components/common/Icon/index.tsx
+++ src/components/common/Icon/index.tsx
@@ -1,16 +1,27 @@
 import { ComponentProps } from 'react';
 
+import { $ } from '@/utils/core';
+
 import { IconName } from './icon.type';
 import { iconMap } from './IconMap';
 
 interface IconProps extends ComponentProps<'svg'> {
   iconName: IconName;
+  width?: number;
+  height?: number;
+  className?: string;
 }
 
-export const Icon = ({ iconName, ...props }: IconProps) => {
+export const Icon = ({
+  iconName,
+  width = 24,
+  height = 24,
+  className,
+  ...props
+}: IconProps) => {
   const path = iconMap[iconName];
   return (
-    <svg {...props}>
+    <svg width={width} height={height} className={$(className)} {...props}>
       <path d={path} />
     </svg>
   );
