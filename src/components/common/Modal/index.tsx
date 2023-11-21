993e023d87d3b0c70f015532155182e0f00dd6aa
HiImConan <mindol0320@naver.com>
2023-11-22 00:29:58 +0900

    feat: modal 컴포넌트 및 커스텀 트랜지션 추가

---
+++ src/components/common/Modal/index.tsx
@@ -0,0 +1,52 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { ReactNode } from 'react';
+
+import { $ } from '@/utils/core';
+
+import { Icon } from '../Icon';
+
+export default function Modal({
+  open,
+  onOpenChange,
+  children,
+}: {
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+  children: ReactNode;
+}) {
+  return (
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      {children}
+    </Dialog.Root>
+  );
+}
+
+function ModalContent({
+  title,
+  className,
+  children,
+}: {
+  title: string;
+  className: string;
+  children: ReactNode;
+}) {
+  return (
+    <Dialog.Portal>
+      <Dialog.Overlay className="fixed inset-0 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]" />
+      <Dialog.Content className={`fixed ${$(className)}`}>
+        <div className="flex items-center justify-between">
+          <Dialog.Title className="text-xl">{title}</Dialog.Title>
+          <Dialog.Close className="text-gray-400 hover:text-gray-500">
+            <Icon iconName="cross" />
+          </Dialog.Close>
+        </div>
+
+        {children}
+      </Dialog.Content>
+    </Dialog.Portal>
+  );
+}
+
+Modal.Trigger = Dialog.Trigger;
+Modal.Content = ModalContent;
+Modal.Close = Dialog.Close;
