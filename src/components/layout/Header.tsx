2ce5ecc82c612054d7538c1ff8f60e90d37930ae
HiImConan <mindol0320@naver.com>
2023-11-21 16:36:29 +0900

    style: 헤더 기초 UI 디자인

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,17 @@
+import { Icon } from '../common/Icon';
+
 export default function Header() {
-  return <div className="h-8"></div>;
+  return (
+    <div className="flex items-center justify-between">
+      <div className="text-3xl font-bold">Match</div>
+
+      <Icon
+        iconName="hamburgerMenu"
+        width={32}
+        height={32}
+        viewBox="0 0 24 24"
+        className="fill-primary"
+      />
+    </div>
+  );
 }
