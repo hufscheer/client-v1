31cd45d1b364d5d4adf3f37d958a4f5bf64d9a8b
HiImConan <mindol0320@naver.com>
2023-11-21 17:02:00 +0900

    feat: 라우트 감지해 헤더 타이틀 자동 교체 기능 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,28 @@
+'use client';
+
+import { useSelectedLayoutSegments } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 import { Icon } from '../common/Icon';
 
 export default function Header() {
+  const segments = useSelectedLayoutSegments();
+  const [titleContent, setTitleContent] = useState<JSX.Element | string>(
+    <Icon iconName="hamburgerMenu" />,
+  );
+
+  useEffect(() => {
+    if (segments.includes('admin')) {
+      setTitleContent('관리자');
+    }
+    if (segments.includes('detail')) {
+      setTitleContent('Match');
+    }
+  }, []);
+
   return (
     <div className="flex items-center justify-between">
-      <div className="text-3xl font-bold">Match</div>
+      <div className="text-3xl font-bold">{titleContent}</div>
 
       <Icon
         iconName="hamburgerMenu"
