1bfb3a8adc49c0b8b755b9dec4bf3f1bf9212c49
HiImConan <mindol0320@naver.com>
2023-11-22 20:05:12 +0900

    chore: 절대경로 alias, props type, classname 정렬

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,19 +1,16 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { ReactNode } from 'react';
 
+import { Icon } from '@/components/common/Icon';
 import { $ } from '@/utils/core';
 
-import { Icon } from '../Icon';
-
-export default function Modal({
-  open,
-  onOpenChange,
-  children,
-}: {
+type ModalProps = {
   open?: boolean;
   onOpenChange?: (open: boolean) => void;
   children: ReactNode;
-}) {
+};
+
+export default function Modal({ open, onOpenChange, children }: ModalProps) {
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       {children}
@@ -33,7 +30,7 @@ function ModalContent({
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]" />
-      <Dialog.Content className={`fixed ${$(className)}`}>
+      <Dialog.Content className={$('fixed', className)}>
         <div className="flex items-center justify-between">
           <Dialog.Title className="text-xl">{title}</Dialog.Title>
           <Dialog.Close className="text-gray-400 hover:text-gray-500">
