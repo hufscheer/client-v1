0b6bc9762565e9e8390bbc4f8e4ebc19b1918ec2
seongminn <berry9901@gmail.com>
2023-11-24 03:24:09 +0900

    chore: type명 변경에 따른 수정 및 instance를 default로 반환

---
+++ src/api/match.ts
@@ -0,0 +1,38 @@
+import {
+  MatchCheerType,
+  MatchCommentType,
+  MatchDetailType,
+  MatchTimelineType,
+} from '@/types/match';
+
+import instance from '.';
+
+export const getMatchById = async (gameId: string) => {
+  const { data } = await instance.get<MatchDetailType>(`/games/${gameId}`);
+
+  return data;
+};
+
+export const getMatchCheerById = async (matchId: string) => {
+  const { data } = await instance.get<MatchCheerType[]>(
+    `/games/${matchId}/cheer`,
+  );
+
+  return data;
+};
+
+export const getGameComments = async (gameId: string, cursor = 1) => {
+  const response = await instance.get<MatchCommentType[]>(
+    `/games/${gameId}/comments?cursor=${cursor}`,
+  );
+
+  return response.data;
+};
+
+export const getMatchTimelineById = async (matchId: string) => {
+  const { data } = await instance.get<MatchTimelineType[]>(
+    `/games/${matchId}/timeline`,
+  );
+
+  return data;
+};
