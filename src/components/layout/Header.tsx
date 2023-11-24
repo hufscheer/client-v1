0f79b8f398107c83feab680c38bc781bc8b900ef
HiImConan <mindol0320@naver.com>
2023-11-24 15:03:50 +0900

    refactor: modal -> sidebar 컴포넌트 교체

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,36 +1,19 @@
 'use client';
 
-import Link from 'next/link';
-import { notFound, useSelectedLayoutSegments } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useSelectedLayoutSegments } from 'next/navigation';
+import { useState } from 'react';
 
-import { getAllLeagues, LeagueType } from '@/api/league';
 import { Icon } from '@/components/common/Icon';
-import Modal from '@/components/common/Modal';
+import Sidebar from '@/components/common/Sidebar';
 import { $ } from '@/utils/core';
 
 export default function Header() {
-  const [menuContent, setMenuContent] = useState<LeagueType[]>([
-    { name: '전체' },
-  ]);
   const [open, setOpen] = useState(false);
-  const isAuthenticated = typeof localStorage.getItem('token') === 'string';
+  const isAuthenticated =
+    typeof window !== 'undefined' &&
+    typeof localStorage.getItem('token') === 'string';
 
-  const handleClickLink = () => {
-    setOpen(false);
-  };
-
-  useEffect(() => {
-    const getLeagueData = async () => {
-      const res = await getAllLeagues();
-
-      if (typeof res === 'number') return notFound();
-
-      setMenuContent(prev => [...prev, ...res]);
-    };
-
-    getLeagueData();
-  }, []);
+  const handleOpenSidebar = () => setOpen(prev => !prev);
 
   return (
     <div
@@ -40,9 +23,8 @@ export default function Header() {
       )}
     >
       <HeaderTitle isAuthenticated={isAuthenticated} />
-
-      <Modal open={open} onOpenChange={setOpen}>
-        <Modal.Trigger>
+      <section>
+        <button onClick={handleOpenSidebar}>
           <Icon
             iconName="hamburgerMenu"
             width={32}
@@ -50,25 +32,9 @@ export default function Header() {
             viewBox="0 0 24 24"
             className={isAuthenticated ? 'fill-white' : 'fill-primary'}
           />
-        </Modal.Trigger>
-
-        <Modal.Content
-          title="대회 목록"
-          className="right-0 top-0 h-screen w-52 space-y-4 bg-white p-5 py-8 data-[state=closed]:animate-[menu-content-hide_200ms] data-[state=open]:animate-[menu-content-show_200ms]"
-        >
-          <div className="flex flex-col gap-2">
-            {menuContent.map(content => (
-              <Link
-                href={{ pathname: '/', query: { leagueId: content.leagueId } }}
-                onClick={handleClickLink}
-                key={content.name}
-              >
-                {content.name}
-              </Link>
-            ))}
-          </div>
-        </Modal.Content>
-      </Modal>
+        </button>
+        {open && <Sidebar onClickChange={handleOpenSidebar} />}
+      </section>
     </div>
   );
 }
