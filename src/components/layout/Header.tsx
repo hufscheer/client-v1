7036eb03c7984e454a640be6bf3b6c2dc1c857e3
HiImConan <mindol0320@naver.com>
2023-11-24 17:31:35 +0900

    refactor: 헤더타이틀 재병합, 시맨틱 태그, 사이드바 open 상태 및 핸들러 재작명, localStorage 보장용 useEffect 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,30 +1,37 @@
 'use client';
 
 import { useSelectedLayoutSegments } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Icon } from '@/components/common/Icon';
 import Sidebar from '@/components/common/Sidebar';
 import { $ } from '@/utils/core';
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
-  const isAuthenticated =
-    typeof window !== 'undefined' &&
-    typeof localStorage.getItem('token') === 'string';
+  const segments = useSelectedLayoutSegments();
+  const titleContent = segments.includes('match') ? 'Match' : '훕치치';
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const handleOpenSidebar = () => setOpen(prev => !prev);
+  const toggleSidebar = () => setIsSidebarOpen(prev => !prev);
+
+  useEffect(() => {
+    setIsAuthenticated(typeof localStorage.getItem('token') === 'string');
+  }, []);
 
   return (
-    <div
+    <header
       className={$(
         'flex items-center justify-between p-4',
         isAuthenticated && 'bg-primary text-white',
       )}
     >
-      <HeaderTitle isAuthenticated={isAuthenticated} />
+      <div className="flex items-baseline gap-1 text-center">
+        <span className="text-3xl font-bold">{titleContent}</span>
+        {isAuthenticated && <span className="">관리자</span>}
+      </div>
       <section>
-        <button onClick={handleOpenSidebar}>
+        <button onClick={toggleSidebar}>
           <Icon
             iconName="hamburgerMenu"
             width={32}
@@ -33,24 +40,8 @@ export default function Header() {
             className={isAuthenticated ? 'fill-white' : 'fill-primary'}
           />
         </button>
-        {open && <Sidebar onClickChange={handleOpenSidebar} />}
+        {isSidebarOpen && <Sidebar onClickSidebar={toggleSidebar} />}
       </section>
-    </div>
-  );
-}
-
-type HeaderTitleProps = {
-  isAuthenticated: boolean;
-};
-function HeaderTitle({ isAuthenticated }: HeaderTitleProps) {
-  const segments = useSelectedLayoutSegments();
-  const titleContent = segments.includes('match') ? 'Match' : '훕치치';
-  // <Icon iconName="logo" />
-
-  return (
-    <div className="flex items-baseline gap-1 text-center">
-      <span className="text-3xl font-bold">{titleContent}</span>
-      {isAuthenticated && <span className="">관리자</span>}
-    </div>
+    </header>
   );
 }
