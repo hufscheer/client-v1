ef385e2ecbba1e9515cf39ffa5f5833c3ea35b4e
HiImConan <mindol0320@naver.com>
2023-11-24 17:29:19 +0900

    chore: onClick 함수명 수정, jsx 정리 및 불필요한 타입체크 삭제

--- src/components/common/Sidebar/index.tsx
+++ src/components/common/Sidebar/index.tsx
@@ -1,23 +1,21 @@
 'use client';
+
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 import { getAllLeagues, LeagueType } from '@/api/league';
 
 type SidebarProps = {
-  onClickChange: () => void;
+  onClickSidebar: () => void;
 };
-
-export default function Sidebar({ onClickChange }: SidebarProps) {
+export default function Sidebar({ onClickSidebar }: SidebarProps) {
   const [menuContent, setMenuContent] = useState<LeagueType[]>([
-    { name: '전체' },
+    { name: '전체', leagueId: 0 },
   ]);
 
   useEffect(() => {
     const getLeagueData = async () => {
       const res = await getAllLeagues();
-
-      if (typeof res === 'number') return;
 
       setMenuContent(prev => [...prev, ...res]);
     };
@@ -26,27 +24,27 @@ export default function Sidebar({ onClickChange }: SidebarProps) {
   }, []);
 
   return (
-    <div>
+    <aside>
       <div
         className="fixed inset-0 bg-black/50"
-        onClick={onClickChange}
+        onClick={onClickSidebar}
         aria-hidden="true"
       />
-      <aside className="fixed right-0 top-0 z-40 h-screen w-64 bg-gray-1 p-5 py-8">
-        <ul className="overflow-y-auto text-gray-5">
-          <div className="my-2 text-xl font-semibold">대회 목록</div>
+      <div className="fixed right-0 top-0 z-40 h-screen w-64 bg-gray-1 p-5 py-8 text-gray-5">
+        <div className="my-2 text-xl font-semibold">대회 목록</div>
+        <ul className="overflow-y-auto ">
           {menuContent.map(content => (
-            <Link
-              href={{ pathname: '/', query: { leagueId: content.leagueId } }}
-              onClick={onClickChange}
-              key={content.name}
-              className="flex items-center rounded-lg p-2 hover:bg-gray-2 dark:text-white dark:hover:bg-gray-5"
-            >
-              {content.name}
-            </Link>
+            <li key={content.name} onClick={onClickSidebar} aria-hidden="true">
+              <Link
+                href={{ pathname: '/', query: { leagueId: content.leagueId } }}
+                className="flex items-center rounded-lg p-2 hover:bg-gray-2 dark:text-white dark:hover:bg-gray-5"
+              >
+                {content.name}
+              </Link>
+            </li>
           ))}
         </ul>
-      </aside>
-    </div>
+      </div>
+    </aside>
   );
 }
