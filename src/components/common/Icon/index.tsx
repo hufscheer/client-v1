cefc10e45ccb38e5239dc027b8ed250922ee4ecf
HiImConan <mindol0320@naver.com>
2023-11-25 21:29:11 +0900

    refactor: 아이콘 svg의 기본값(width, height, viewbox) 및 여러개 path 케이스 맵핑

--- src/components/common/Icon/index.tsx
+++ src/components/common/Icon/index.tsx
@@ -9,25 +9,24 @@ interface IconProps extends ComponentProps<'svg'> {
   iconName: IconName;
 }
 
-export const Icon = ({
-  iconName,
-  width = 24,
-  height = 24,
-  className,
-  ...props
-}: IconProps) => {
-  const path = iconMap[iconName];
+export const Icon = ({ iconName, className, ...props }: IconProps) => {
+  const { path, width, height, viewBox } = iconMap[iconName];
   return (
     <svg
       width={width}
       height={height}
+      viewBox={viewBox}
       fillRule="evenodd"
       clipRule="evenodd"
       xmlns="http://www.w3.org/2000/svg"
       className={$(className)}
       {...props}
     >
-      <path d={path} />
+      {Array.isArray(path) ? (
+        path.map((p, i) => <path d={p} key={i} />)
+      ) : (
+        <path d={path} />
+      )}
     </svg>
   );
 };
