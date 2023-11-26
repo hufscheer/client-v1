18141c4b80bc0265f29fd30ba718f5338e7e3cd9
HiImConan <mindol0320@naver.com>
2023-11-27 01:44:30 +0900

    chore: fill-rule, clip-rule 카멜케이스로 변경

--- src/components/common/Icon/svg/PlusCircled.tsx
+++ src/components/common/Icon/svg/PlusCircled.tsx
@@ -12,8 +12,8 @@ export const PlusCircled = ({
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M10.5 0C4.704 0 0 4.704 0 10.5C0 16.296 4.704 21 10.5 21C16.296 21 21 16.296 21 10.5C21 4.704 16.296 0 10.5 0ZM15.75 11.55H11.55V15.75H9.45V11.55H5.25V9.45H9.45V5.25H11.55V9.45H15.75V11.55Z"
         fill="currentColor"
       />
