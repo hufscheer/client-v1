fb179d1bf1918a3aa41659cc8a17c9e246fa24f7
HiImConan <mindol0320@naver.com>
2023-11-29 02:06:49 +0900

    chore: useDate 훅 삭제

--- src/hooks/useDate.ts
+++
@@ -1,9 +0,0 @@
-export default function useDate(date: string) {
-  const currentDate = new Date(date);
-  return {
-    month: currentDate.getMonth() + 1,
-    day: currentDate.getDate(),
-    hour: currentDate.getHours() + 9,
-    minute: currentDate.getMinutes(),
-  };
-}
