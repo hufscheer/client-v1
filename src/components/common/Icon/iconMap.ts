6b363ee1227db28b5f54cc41190de99b641137eb
HiImConan <mindol0320@naver.com>
2023-11-29 02:04:52 +0900

    feat: CheckCircled 아이콘 추가

--- src/components/common/Icon/iconMap.ts
+++ src/components/common/Icon/iconMap.ts
@@ -2,6 +2,7 @@ import { BackgroundLogo } from './svg/BackgroundLogo';
 import { Calendar } from './svg/Calendar';
 import { CaretDown } from './svg/CaretDown';
 import { CaretUp } from './svg/CaretUp';
+import { CheckCircled } from './svg/CheckCircled';
 import { Clip } from './svg/Clip';
 import { Cross } from './svg/Cross';
 import { HamburgerMenu } from './svg/HamburgerMenu';
@@ -20,6 +21,7 @@ export const iconMap = {
   calendar: Calendar,
   caretDown: CaretDown,
   caretUp: CaretUp,
+  checkCircled: CheckCircled,
   clip: Clip,
   cross: Cross,
   hamburgerMenu: HamburgerMenu,
