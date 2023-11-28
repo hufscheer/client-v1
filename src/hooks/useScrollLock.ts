e0f914d22150c4b076f4fa93ab057415c641a900
HiImConan <mindol0320@naver.com>
2023-11-29 00:41:16 +0900

    feat: useScrollLock 훅 추가

---
+++ src/hooks/useScrollLock.ts
@@ -0,0 +1,20 @@
+export function useScrollLock() {
+  const scrollPosition = window.scrollY;
+
+  const disableScroll = () => {
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollPosition}px`;
+    document.body.style.width = '100%';
+  };
+
+  const enableScroll = () => {
+    document.body.style.removeProperty('overflow');
+    document.body.style.removeProperty('position');
+    document.body.style.removeProperty('top');
+    document.body.style.removeProperty('width');
+    window.scrollTo(0, scrollPosition);
+  };
+
+  return { disableScroll, enableScroll };
+}
