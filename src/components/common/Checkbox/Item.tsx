b37ebb9ca95d5210617fc02a80bf910cd7b17332
HiImConan <mindol0320@naver.com>
2023-11-29 01:33:32 +0900

    feat: 리그 등록/수정 페이지 컴포넌트, 버튼 컴포넌트 추가

---
+++ src/components/common/Checkbox/Item.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { ComponentProps, useRef } from 'react';
+
+import { $ } from '@/utils/core';
+
+import Button from '../Button';
+import { Icon } from '../Icon';
+
+export default function CheckboxItem({
+  id,
+  name,
+  value,
+  children,
+  checked,
+  onChange,
+}: ComponentProps<'input'>) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onClick = () => {
+    if (!inputRef.current) return;
+
+    inputRef.current.click();
+  };
+
+  return (
+    <>
+      <Button
+        className={$(
+          'mt-2 w-full gap-2 rounded-lg border border-gray-3/70 p-4 transition-colors duration-100',
+          checked ? 'bg-[#0718AE] text-white' : 'bg-secondary/25',
+        )}
+        type="button"
+        onClick={onClick}
+      >
+        {children}
+        {checked && <Icon iconName="checkCircled" className="fill-white/50" />}
+      </Button>
+
+      <input
+        name={name}
+        id={id}
+        type="checkbox"
+        value={value}
+        checked={checked}
+        ref={inputRef}
+        onChange={onChange}
+        className="hidden"
+      />
+    </>
+  );
+}
