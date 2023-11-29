1d13e7460b6e92c66bc4bc2481c1f28378028997
seongminn <berry9901@gmail.com>
2023-11-29 18:44:05 +0900

    chore: sentry dsn env 파일에 추가

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,11 +3,10 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://cc1dcf1845d69a9079c615d462e122b2@o4506026798088192.ingest.sentry.io/4506026816503808",
-
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
