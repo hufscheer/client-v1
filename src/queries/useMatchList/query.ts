31dea516e1ce43e01d3ab79d85999e2278dbfce2
seongminn <berry9901@gmail.com>
2023-12-01 15:39:04 +0900

    fix: 서버 문제로 인한 무한 스크롤 제거

--- src/queries/useMatchList/query.ts
+++ src/queries/useMatchList/query.ts
@@ -1,4 +1,4 @@
-import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { getMatchList, MatchListParams } from '@/api/match';
 
@@ -7,25 +7,18 @@ export const useMatchList = ({
   status = 'playing',
   league_id,
 }: Omit<MatchListParams, 'cursor' | 'size'>) => {
-  const { data, error, isFetching, hasNextPage, fetchNextPage } =
-    useSuspenseInfiniteQuery({
-      queryKey: ['match-list', sport_id, status, league_id],
-      queryFn: ({ pageParam }) =>
-        getMatchList({
-          sport_id,
-          status,
-          league_id,
-          cursor: pageParam,
-        }),
-      initialPageParam: 0,
-      getNextPageParam: lastPage => lastPage.at(-1)?.id || null,
-    });
+  const { data, error } = useSuspenseQuery({
+    queryKey: ['match-list', sport_id, status, league_id],
+    queryFn: () =>
+      getMatchList({
+        sport_id,
+        status,
+        league_id,
+      }),
+  });
 
   return {
     matchList: data,
     error,
-    isFetching,
-    hasNextPage,
-    fetchNextPage,
   };
 };
